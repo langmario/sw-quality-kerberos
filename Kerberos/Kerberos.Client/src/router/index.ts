721a2af72aaad89d49fe2bd671d9140d987d37e0
Mivoe13xy0 <git@micha-b.de>
2021-05-15 17:44:51 +0200

    basic routing

--- Kerberos/Kerberos.Client/src/router/index.ts
+++ Kerberos/Kerberos.Client/src/router/index.ts
@@ -1,19 +1,29 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Default from '../views/Default.vue'
+import Languages from '../views/Languages.vue'
+import Title from '../views/Title.vue'
+import Salutation from '../views/Salutation.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Default',
+    component: Default
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/language',
+    name: 'Languages',
+    component: Languages
+  },
+  {
+    path: '/salutation',
+    name: 'Salutation',
+    component: Salutation
+  },
+  {
+    path: '/title',
+    name: 'Title',
+    component: Title
   }
 ]
 
