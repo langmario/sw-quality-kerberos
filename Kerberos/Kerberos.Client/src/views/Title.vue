d9f3088712c91eb2b1f0e3707ee6b9e6117ab233
Mivoe13xy0 <git@micha-b.de>
2021-05-15 19:23:54 +0200

    initial client

---
+++ Kerberos/Kerberos.Client/src/views/Title.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <p>Title</p>
+    </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.component("Title", {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+})
+</script>
+
+<style lang="scss">
+
+</style>
