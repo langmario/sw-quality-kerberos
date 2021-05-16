77ee6cc1994934dda63f165c642a554f68d0782f
Mivoe13xy0 <git@micha-b.de>
2021-05-16 13:34:23 +0200

    added basic language page

--- Kerberos/Kerberos.Client/src/views/Language.vue
+++ Kerberos/Kerberos.Client/src/views/Language.vue
@@ -1,102 +1,207 @@
 <template>
-<section>
-<h1 class="mb-5">Language</h1>
-    <b-card header="Sprachen hinzufügen">
-        <b-form @submit="onSubmit">
-            <div class="row">
-                <div class="col">
-                    <b-form-group id="input-key" label="Sprachschlüssel" label-for="key-input">
-                        <b-input id="key-input" v-model="formLanguage.key" type="text" required placeholder="de-DE"></b-input>
-                    </b-form-group>
+    <section>
+        <h1 class="mb-5">Sprachen</h1>
+        <b-modal
+            id="form-modal"
+            title="Sprache hinzufügen"
+            centered
+            v-model="showModal"
+            size="lg"
+            ok-title="Absenden"
+            ok-variant="success"
+            cancel-title="Abbrechen"
+            @ok="onModalSubmit"
+        >
+            <b-form>
+                <!-- @submit="onSubmit" -->
+                <div class="row">
+                    <div class="col">
+                        <b-form-group
+                            id="input-key"
+                            label="Sprachschlüssel"
+                            label-for="key-input"
+                        >
+                            <b-input
+                                id="key-input"
+                                v-model="formItem.key"
+                                type="text"
+                                required
+                                placeholder="de-DE"
+                            ></b-input>
+                        </b-form-group>
+                    </div>
+                    <div class="col">
+                        <b-form-group
+                            id="input-name"
+                            label="Name"
+                            label-for="name-input"
+                        >
+                            <b-input
+                                id="name-input"
+                                v-model="formItem.name"
+                                type="text"
+                                required
+                                placeholder="Deutsch"
+                            ></b-input>
+                        </b-form-group>
+                    </div>
                 </div>
-                <div class="col">
-                    <b-form-group id="input-name" label="Name" label-for="name-input">
-                        <b-input id="name-input" v-model="formLanguage.name" type="text" required placeholder="Deutsch"></b-input>
-                    </b-form-group>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <b-button type="submit" variant="success">Sprache hinzufügen</b-button>
-                </div>
-            </div>
-        </b-form>
-    </b-card>
-    <div class="mt-3">
-        <div v-if="loading">
-            Lade Daten. Bitte warten...<br><br>
-        <b-spinner/>
+            </b-form>
+        </b-modal>
+
+        <div class="mt-3">
+            <span class="mr-2">Optionen:</span>
+            <b-button-group>
+                <b-button variant="outline-success" @click="addButtonClicked"
+                    >Neues Element hinzufügen</b-button
+                >
+                <b-button variant="outline-danger" @click="deleteButtonClicked"
+                    >Ausgewältes Element löschen</b-button
+                >
+            </b-button-group>
         </div>
-        <b-table v-if="languageItems.length != 0" bordered hover striped :items="languageItems">
-        </b-table>
-        <p v-if="languageItems.length == 0 && !loading && !error" class="text-center">Keine Daten vorhanden</p>
-        <b-card
-            v-if="error"
-            border-variant="danger"
-            header="Es ist ein Fehler aufgetreten"
-            header-border-variant="danger"
-            header-text-variant="danger"
-            align="center">
-            <b-card-text>
-                {{errorMsg}}
-            </b-card-text>
-        </b-card>
-    </div>
-</section>
+
+        <div class="mt-3">
+            <div v-if="loading">
+                Lade Daten. Bitte warten...<br /><br />
+                <b-spinner />
+            </div>
+            <b-table
+                v-if="items.length != 0"
+                bordered
+                hover
+                striped
+                :items="items"
+                :fields="fields"
+                select-mode="single"
+                responsive="sm"
+                ref="selectableTable"
+                selectable
+                @row-selected="onRowSelected"
+            >
+            </b-table>
+            <p
+                v-if="items.length == 0 && !loading && !error"
+                class="text-center"
+            >
+                Keine Daten vorhanden
+            </p>
+            <b-card
+                v-if="error"
+                border-variant="danger"
+                header="Es ist ein Fehler aufgetreten"
+                header-border-variant="danger"
+                header-text-variant="danger"
+                align="center"
+            >
+                <b-card-text>
+                    {{ errorMsg }}
+                </b-card-text>
+            </b-card>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
-import axios from 'axios'
-import Vue from 'vue'
-import {Language} from '../models/models'
-const {VUE_APP_API_BASE_URL} = process.env
+import axios from "axios";
+import Vue from "vue";
+import { Language } from "../models/models";
+const { VUE_APP_API_BASE_URL } = process.env;
 
 export default Vue.component("Language", {
     data() {
         return {
             loading: false,
-            languageItems: [] as Language[],
+            items: [] as Language[],
+            fields: [
+                { key: "key", sortable: true, label: "Sprachschlüssel" },
+                { key: "name", sortable: true, label: "Sprachname" }
+            ],
             error: false,
             errorMsg: "Fehler...",
-            formLanguage: {} as Language
-        }
+            formItem: {} as Language,
+            selectedItem: {} as Language,
+            showModal: false
+        };
     },
     mounted() {
         this.loadLangaugeItems();
     },
     methods: {
-        loadLangaugeItems(){
+        loadLangaugeItems() {
             this.loading = true;
             const url = VUE_APP_API_BASE_URL + "/api/languages";
-            axios.get(url).then(response => {
-                this.languageItems = response.data;
-                this.loading = false;
-            }).catch(error => {
-                console.error(error);
-                this.loading = false;
-                this.error = true;
-                this.errorMsg = error;
-            });
+            axios
+                .get(url)
+                .then(response => {
+                    this.items = response.data;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.error(error);
+                    this.loading = false;
+                    this.error = true;
+                    this.errorMsg = error;
+                });
         },
-        onSubmit(event: any){
-            event.preventDefault();
+        inputValid() {
+            return this.formItem.key != "" && this.formItem.name != "";
+        },
+        onModalSubmit(event: any) {
+            if (!this.inputValid()) {
+                event.preventDefault();
+                return;
+            }
+
             const url = VUE_APP_API_BASE_URL + "/api/languages";
-            console.log(url)
-            axios.post(url, this.formLanguage).then(response => {
-                this.formLanguage.name = '';
-                this.formLanguage.key = '';
-                console.log(response)
-                const language: Language = response.data
-                this.languageItems.push(language)
-            }).catch(error => {
-                // TODO: error handling
-                console.error(error);
-            })
+            console.log(url);
+            axios
+                .post(url, this.formItem)
+                .then(response => {
+                    const language = response.data;
+                    this.items.push(language);
+                })
+                .catch(error => {
+                    this.showErrorMessage(error);
+                    console.error(error);
+                });
+        },
+        addButtonClicked(event: any) {
+            this.showModal = !this.showModal;
+            this.formItem = {} as Language;
+            this.formItem.id = 0;
+            this.formItem.key = "";
+            this.formItem.name = "";
+        },
+        deleteButtonClicked(event: any) {
+            if (!this.selectedItem || !this.selectedItem.id) return;
+
+            const url =
+                VUE_APP_API_BASE_URL + "/api/languages/" + this.selectedItem.id;
+            axios
+                .delete(url)
+                .then(response => {
+                    if (response.status == 200) {
+                        const index = this.items.indexOf(this.selectedItem);
+                        this.items.splice(index, 1);
+                    } else {
+                        // TODO: error handling
+                    }
+                })
+                .catch(error => {
+                    this.showErrorMessage(error);
+                    console.error(error);
+                });
+        },
+        onRowSelected(item: Language[]) {
+            this.selectedItem = item[0];
+        },
+        showErrorMessage(message: string){
+            this.error = true;
+            this.errorMsg = message;
         }
     }
-})
+});
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
